7b1fb24dac53e2cacaab1eef34cc3dd955ddae6d
homo <nfu40043205@gmail.com>
2024-04-16 01:15:12 +0800

    test update

---
+++ assets/js/header.js
@@ -0,0 +1,15 @@
+document.write( '<!-- Header -->\n' );
+document.write( '	<header id=\"header\">\n' );
+document.write( '		<h1><a href=\"#\">Wnag <span>Family</span></a></h1>\n' );
+document.write( '		<a href=\"#menu\">Menu</a>\n' );
+document.write( '	</header>\n' );
+document.write( '\n' );
+document.write( '	<!-- Nav -->\n' );
+document.write( '	<nav id=\"menu\">\n' );
+document.write( '		<ul class=\"links\">\n' );
+document.write( '			<li><a href=\"Index.html\">首頁</a></li>\n' );
+document.write( '			<li><a href=\"generic.html\">固定材料/器具</a></li>\n' );
+document.write( '			<li><a href=\"BabyIndex.html\">幼兒專區</a></li>\n' );
+document.write( '			<li><a href=\"elements.html\">待定</a></li>\n' );
+document.write( '		</ul>\n' );
+document.write( '	</nav>' );
